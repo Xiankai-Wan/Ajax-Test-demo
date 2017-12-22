d5265632c6f2f8f6bfea344a65d226eca789757b
Kevin-Wan <xiankai.wan@gmail.com>
2017-12-22 14:25:12 +0800

    Add : add the jQuery ajax solution

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -10,5 +10,35 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+        function addImage(images) {
+            const firstImage = images.results[0];
+
+            responseContainer.insertAdjacentHTML('afterbegin', `<figure>
+            <img src="${firstImage.urls.small}" alt="${searchedForText}">
+            <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+        </figure>`
+            );
+        }
+        $.ajax({
+            url:`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+            headers:{
+                Authorization:'Client-ID 487871313476e5af6dfc3100a2692728d6f58fb8beb43b17d301663d832a5cb9'
+            }
+        }
+        ).done(addImage);
+        function addArticles(article) {
+            responseContainer.insertAdjacentHTML('beforeend', '<ul>' +
+                article.response.docs.map(a =>
+                    `<li class="article">
+                            <h2><a href="${a.web_url}">${a.headline.main}</a></h2>
+                            <p>${a.snippet}</p>
+                            </li>`).join('')
+                + '</ul>');
+        }
+        $.ajax(
+            {
+                url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=61dcbc55d0084951acd83dc785a5b97a`,  
+            }
+        ).done(addArticles);
     });
 })();
